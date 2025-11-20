b0509c41961f4a555a5bfaff2767496e91586f4a
Ivan Boboshko <ivanboboshko888@gmail.com>
2025-11-20 22:41:43 +0300

    refactor models structure (rebased)

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,9 +1,11 @@
 from src.model.baseline_model import BaselineModel
-from src.model.unet import UNet
+from src.model.dprnn.dprnn import DPRNN
+from src.model.unet.unet import UNet
 from src.model.rtfs_net_layers import RTFSNet
 
 __all__ = [
     "BaselineModel",
     "UNet",
+    "DPRNN",
     "RTFSNet",
 ]
