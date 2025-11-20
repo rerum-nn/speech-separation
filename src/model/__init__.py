af447ad69327186b7b0059f9df12012f5e8a4365
Ivan Boboshko <ivanboboshko888@gmail.com>
2025-11-20 22:41:43 +0300

    refactor models structure

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,7 +1,9 @@
 from src.model.baseline_model import BaselineModel
-from src.model.unet import UNet
+from src.model.dprnn.dprnn import DPRNN
+from src.model.unet.unet import UNet
 
 __all__ = [
     "BaselineModel",
     "UNet",
+    "DPRNN",
 ]
